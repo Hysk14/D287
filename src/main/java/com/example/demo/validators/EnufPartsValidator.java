1bd771b34beb20e3f4c84a7ff97c72f91845ec5d
Hysk14 <straightliving14@gmail.com>
2025-06-23 19:55:26 -1000

    - File Name: EnufPartsValidator.java   Line Numbers: 36-42   Changes: added if statement to check if product has sufficient parts to purchase;   else throw custom error message

--- src/main/java/com/example/demo/validators/EnufPartsValidator.java
+++ src/main/java/com/example/demo/validators/EnufPartsValidator.java
@@ -33,7 +33,13 @@ public class EnufPartsValidator implements ConstraintValidator<ValidEnufParts, P
         if (product.getId() != 0) {
             Product myProduct = repo.findById((int) product.getId());
             for (Part p : myProduct.getParts()) {
-                if (p.getInv()<(product.getInv()-myProduct.getInv()))return false;
+                if (p.getInv() - product.getInv() < p.getMinInv()){
+                    constraintValidatorContext.disableDefaultConstraintViolation();
+                    constraintValidatorContext.buildConstraintViolationWithTemplate(
+                            "Low " + p.getName() + " inventory. " + "Minimum required: " + p.getMinInv()
+                    ).addConstraintViolation();
+                    return false;
+                }
             }
             return true;
         }
